f693866df49f4f65fb11b668c970807458b12891
성현 <mingyov0807@gmail.com>
2020-08-26 17:26:52 +0900

    node api ver2

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const morgan = require('morgan');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+
+const app = express();
+
+passportConfig();
+
+app.set('port',process.env.PORT || 8002);
+app.set('view engine', 'html');
+nunjucks.configure('views',{
+    express:app,
+    watch:true,
+});
+
+
